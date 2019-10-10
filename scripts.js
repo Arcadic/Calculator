795d0cd49c90d4ba60836c75c07d7abd65992684
Tim Gottman <timothy.gottman@student.swic.edu>
2019-10-09 20:56:59 -0500

    Add bracket notation to use output

--- scripts.js
+++ scripts.js
@@ -6,31 +6,32 @@
  */
 
 const calculator = {
-  add: function add(a, b) {
+  add(a, b) {
     return Number(a) + Number(b);
   },
-  subtract: function subtract(a, b) {
+  subtract(a, b) {
     return a - b;
   },
-  multiply: function multiply(a, b) {
+  multiply(a, b) {
     return a * b;
   },
-  divide: function divide(a, b) {
+  divide(a, b) {
     return a / b;
   },
-  remainder: function remainder(a, b) {
+  remainder(a, b) {
     return a % b;
   }
 };
-
-console.log(calculator.add(1, 2));
 
 const form = document.querySelector("form");
 const num1 = document.querySelector("#num-1");
 const num2 = document.querySelector("#num-2");
 const select = document.querySelector("select");
+const output = document.querySelector("output");
+//bracket notation
+output.textContent = calculator["add"](2, 5);
 
-form.addEventListener("submit", function(event) {
+output.form.addEventListener("submit", function(event) {
   event.preventDefault();
   console.log(num1.value, num2.value, select.value);
 });
